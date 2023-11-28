ea3335b1cfa05ad6490b21386a36bb91ce08ecaf
Rubens Gabriel <rubens-ita@hotmail.com.br>
2023-11-28 18:49:27 -0300

    ajustes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,7 @@
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-//import { FormsModule } from '@angular/forms'; 
 import { ReactiveFormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListarAlunosComponent } from './listar-alunos/listar-alunos.component';
@@ -20,7 +17,6 @@ import { EdicaoAlunoComponent } from './edicao-aluno/edicao-aluno.component';
   ],
   imports: [
     BrowserModule,
-    //FormsModule, 
     AppRoutingModule,
     ReactiveFormsModule
   ],
